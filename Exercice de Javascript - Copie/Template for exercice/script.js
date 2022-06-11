2d3551e3b164f2377b08aa123b3f338679be3a72
Julien Esnault <troxydev@gmail.com>
2022-06-11 14:14:32 +0200

    11 Juin Debut du projet 30 Days JS

--- Exercice de Javascript - Copie/Template for exercice/script.js
+++ Exercice de Javascript - Copie/Template for exercice/script.js
@@ -1,16 +1,27 @@
-// Callback Function Example
+const log = document.getElementById("log");
 
-function great(name, callback) {
-  console.log("Hey Word l");
-  // callback function
-  // executed only after the greet() is executed
-  callback(name);
-}
-// callback function
-function callback(name) {
-  console.log("Hey" + name);
+document.addEventListener("keydown", logKey);
+
+function logKey(e) {
+  log.textContent += ` ${e.code}`;
 }
 
-// calling the function after 2 seconds
-
-setTimeout(great, 2000, "John", callback);
+const log = 0;
+switch (toto) {
+    case -1:
+        console.log('moins un');
+        break;
+    case 0: // toto vaut 0 donc ce cas correspond
+        console.log(0);
+        // NOTE : le break aurait du être placé ici
+    case 1: // pas de break pour 'case 0:' les instructions de ce cas sont
+            // exécutées aussi
+        console.log(1);
+        break; // on a un break a ce niveau donc les instructions
+               // des cas suivants ne seront pas exécutées
+    case 2:
+        console.log(2);
+        break;
+    default:
+        console.log('default');
+}
---
+++ Javascript 30 Day Exercice/01 - JavaScript Drum Kit/script.js
@@ -0,0 +1,128 @@
+// A key
+document.addEventListener("keydown", function (KeyboardEvent) {
+  // I listen all my document("HTML")
+  if (KeyboardEvent.keyCode == 65) {
+    document.getElementById(65).play();
+    var element = document.querySelector('[data-key="65"]');
+    element.classList.add("playing");
+    const myTime = setTimeout(remove, 200);
+    function remove() {
+      element.classList.remove("playing");
+    }
+  }
+});
+document.addEventListener("keydown", function (KeyboardEvent) {
+  // I listen all my document("HTML")
+  if (KeyboardEvent.keyCode == 83) {
+    document.getElementById(83).play();
+    var element = document.querySelector('[data-key="83"]');
+    element.classList.add("playing");
+    const myTime = setTimeout(remove, 200);
+    function remove() {
+      element.classList.remove("playing");
+    }
+  }
+});
+document.addEventListener("keydown", function (KeyboardEvent) {
+  // I listen all my document("HTML")
+  if (KeyboardEvent.keyCode == 68) {
+    document.getElementById(68).play();
+    var element = document.querySelector('[data-key="68"]');
+    element.classList.add("playing");
+    const myTime = setTimeout(remove, 200);
+    function remove() {
+      element.classList.remove("playing");
+    }
+  }
+});
+document.addEventListener("keydown", function (KeyboardEvent) {
+  // I listen all my document("HTML")
+  if (KeyboardEvent.keyCode == 70) {
+    document.getElementById(70).play();
+    var element = document.querySelector('[data-key="70"]');
+    element.classList.add("playing");
+    const myTime = setTimeout(remove, 200);
+    function remove() {
+      element.classList.remove("playing");
+    }
+  }
+});
+document.addEventListener("keydown", function (KeyboardEvent) {
+  // I listen all my document("HTML")
+  if (KeyboardEvent.keyCode == 71) {
+    document.getElementById(71).play();
+    var element = document.querySelector('[data-key="71"]');
+    element.classList.add("playing");
+    const myTime = setTimeout(remove, 200);
+    function remove() {
+      element.classList.remove("playing");
+    }
+  }
+});
+document.addEventListener("keydown", function (KeyboardEvent) {
+  // I listen all my document("HTML")
+  if (KeyboardEvent.keyCode == 71) {
+    document.getElementById(71).play();
+    var element = document.querySelector('[data-key="71"]');
+    element.classList.add("playing");
+    const myTime = setTimeout(remove, 200);
+    function remove() {
+      element.classList.remove("playing");
+    }
+  }
+});
+document.addEventListener("keydown", function (KeyboardEvent) {
+  // I listen all my document("HTML")
+  if (KeyboardEvent.keyCode == 72) {
+    document.getElementById(72).play();
+    var element = document.querySelector('[data-key="72"]');
+    element.classList.add("playing");
+    const myTime = setTimeout(remove, 200);
+    function remove() {
+      element.classList.remove("playing");
+    }
+  }
+});
+document.addEventListener("keydown", function (KeyboardEvent) {
+  // I listen all my document("HTML")
+  if (KeyboardEvent.keyCode == 74) {
+    document.getElementById(74).play();
+    var element = document.querySelector('[data-key="74"]');
+    element.classList.add("playing");
+    const myTime = setTimeout(remove, 200);
+    function remove() {
+      element.classList.remove("playing");
+    }
+  }
+});
+document.addEventListener("keydown", function (KeyboardEvent) {
+  // I listen all my document("HTML")
+  if (KeyboardEvent.keyCode == 75) {
+    document.getElementById(75).play();
+    var element = document.querySelector('[data-key="75"]');
+    element.classList.add("playing");
+    const myTime = setTimeout(remove, 200);
+    function remove() {
+      element.classList.remove("playing");
+    }
+  }
+});
+
+document.addEventListener("keydown", function (KeyboardEvent) {
+  // I listen all my document("HTML")
+  if (KeyboardEvent.keyCode == 76) {
+    document.getElementById(76).play();
+    var element = document.querySelector('[data-key="76"]');
+    element.classList.add("playing");
+    const myTime = setTimeout(remove, 200);
+    function remove() {
+      element.classList.remove("playing");
+    }
+  }
+});
+
+// const key = document.querySelector('[data-key="65"]');
+// console.log(key);
+// switch (key) {
+// }
+
